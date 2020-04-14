b4f874bb588b462ccefca53fdae08ba642881c48
trapezechic <ingridhoffman@me.com>
2020-04-14 16:28:27 -0700

    wrote server file with basic router

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,8 @@
+// Node dependencies
+const express = require("express");
+
+// Set router
+const router = express.Router();
+
+// Export
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,27 @@
+// Node dependencies
 const express = require("express");
+
+// Set express app and port
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+// Serve static content from the "public" directory
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set handlebars
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes for server
+const routes = require("./controllers/burgers_controller");
+app.use(routes);
+
+// Start server
+app.listen(PORT, () => {
+	console.log("Server listening on: http://localhost:" + PORT);
+});
