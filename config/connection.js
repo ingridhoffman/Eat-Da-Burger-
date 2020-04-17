7b2bf57364c9b4b9c45adbccde3e8730080c8cb2
trapezechic <ingridhoffman@me.com>
2020-04-16 18:34:57 -0700

    JawsDB connection for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
 // Node dependencies
 const mysql = require("mysql");
 
+let connection;
 // Connection parameters
-const connection = mysql.createConnection({
-	host: "localhost",
-	port: 3306,
-	user: "root",
-	password: "UofOBootcamp",
-	database: "burgers_db",
-});
-
+// Use JawsDB for Heroku deployment
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	const connection = mysql.createConnection({
+		host: "localhost",
+		port: 3306,
+		user: "root",
+		password: "UofOBootcamp",
+		database: "burgers_db",
+	});
+}
 // Create connection
 connection.connect((err) => {
 	if (err) {
